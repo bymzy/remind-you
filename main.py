5bf489a41addf08130faf0c88d6334ef8bb2fab1
mingzhengying <bymzy@oulook.com>
2017-07-15 23:43:02 +0800

    add daemon

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*- 
 
 
+import daemon
 from scheduler import scheduler_loop
 
 if __name__ == "__main__":
-    scheduler_loop()
+    with daemon.DaemonContext():
+        scheduler_loop()
 
